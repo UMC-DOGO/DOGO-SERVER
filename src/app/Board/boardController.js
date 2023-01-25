1406d1b2eecbb23250e9d0e42fb78fb193b48087
youngduck <dudejr5839@naver.com>
2023-01-25 22:12:02 +0900

    중간상황

---
+++ src/app/Board/boardController.js
@@ -0,0 +1,65 @@
+const boardService = require("../../app/Board/boardService");
+const baseResponse = require("../../../config/baseResponseStatus");
+const { response, errResponse } = require("../../../config/response");
+
+// 0. 게시글 작성
+//카테고리 추가해야함
+exports.postBoards = async function (req, res) {
+  const { title, content, img } = req.body;
+  //제목 체크
+  if (!title) return res.send(errResponse(baseResponse.BOARD_TITLE_EMPTY));
+  //내용 체크
+  if (!content) return res.send(errResponse(baseResponse.BOARD_CONTENT_EMPTY));
+  //가격 체크
+
+  const postBoardsResponse = await boardService.createBoard(
+    title,
+    content,
+    img
+  );
+  return res.send(postBoardsResponse);
+};
+
+// 1. 게시글 조회
+exports.getBoards = async function (req, res) {
+  const boardListResult = await boardService.retrieveBoardList();
+  return res.send(response(baseResponse.SUCCESS, boardListResult));
+};
+
+// 2. 게시글 세부 조회
+exports.getBoard = async function (req, res) {
+  const boardId = req.params.boardId;
+  if (!boardId) return res.send(errResponse(baseResponse.BOARD_BOARDID_EMPTY));
+
+  const boardResult = await boardService.retrieveBoard(boardId);
+  return res.send(response(baseResponse.SUCCESS, boardResult));
+};
+
+// 3. 게시글 삭제
+exports.deleteBoard = async function (req, res) {
+  const boardId = req.params.boardId;
+  const deleteBoard = await boardService.deleteBoard(boardId);
+
+  return res.send(deleteBoard);
+};
+
+// 4. 게시글 수정
+exports.patchBoard = async function (req, res) {
+  // const title = req.params.title
+  // const content = req.params.content
+  // const price = req.params.price
+  const boardId = req.params.boardId;
+  const { title, content, img } = req.body;
+  //제목 체크
+  if (!title) return res.send(errResponse(baseResponse.BOARD_TITLE_EMPTY));
+  //내용 체크
+  if (!content) return res.send(errResponse(baseResponse.BOARD_CONTENT_EMPTY));
+
+  const editBoardInfo = await boardService.editBoard(
+    boardId,
+    title,
+    content,
+    img
+  );
+  return res.send(editBoardInfo);
+};
---
+++ src/app/Board/boardRoute.js
@@ -0,0 +1,15 @@
+module.exports = function (app) {
+  const board = require("./boardController");
+
+  // 0. 커뮤니티 작성
+  app.post("/board", board.postBoards);
+  // 1. 커뮤니티 조회
+  app.get("/board", board.getBoards);
+
+  // 2. 커뮤니티 세부 조회
+  app.get("/board/:boardId", board.getBoard);
+  // 3. 커뮤니티 삭제
+  app.delete("/board/:boardId", board.deleteBoard);
+  // 4. 커뮤니티 수정
+  app.patch("/board/:boardId", board.patchBoard);
+};
