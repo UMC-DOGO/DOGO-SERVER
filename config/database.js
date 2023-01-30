6c00db1b16efd5efc028e5502d0264174af0b164
youngduck <dudejr5839@naver.com>
2023-01-30 19:32:25 +0900

    database 충돌해결

--- config/database.js
+++ config/database.js
@@ -6,13 +6,8 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   port: "3306",
-<<<<<<< Updated upstream
   password: "1234",
   database: "market",
-=======
-  password: "kelly1530*",
-  database: "minju",
->>>>>>> Stashed changes
 });
 
 module.exports = {
