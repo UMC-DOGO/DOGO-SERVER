ac6d7b4453f26e2538a99818a794dc6c757047f1
youngduck <dudejr5839@naver.com>
2023-02-10 15:59:58 +0900

    jwt payload 조정

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -2,10 +2,10 @@ const jwtMiddleware = require("../../../config/jwtMiddleware");
 const userProvider = require("../../app/User/userProvider");
 const userService = require("../../app/User/userService");
 const baseResponse = require("../../../config/baseResponseStatus");
-const {response, errResponse} = require("../../../config/response");
+const { response, errResponse } = require("../../../config/response");
 
 const regexEmail = require("regex-email");
-const {emit} = require("nodemon");
+const { emit } = require("nodemon");
 
 /**
  * API No. 0
@@ -22,40 +22,59 @@ const {emit} = require("nodemon");
  * [POST] /app/users
  */
 exports.postUsers = async function (req, res) {
+  /**
+   * Body: email, password, nickname
+   */
+  const {
+    nickname,
+    address,
+    age,
+    gender,
+    breed,
+    dogAge,
+    introduce,
+    profileImage,
+    status,
+    email,
+    password,
+  } = req.body;
 
-    /**
-     * Body: email, password, nickname
-     */
-    const {nickname, address, age, gender, breed, dogAge, introduce, profileImage,status, email, password} = req.body;
+  // 빈 값 체크
+  if (!email) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
+  if (!introduce) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
+  if (!nickname) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
 
-    // 빈 값 체크
-    if (!email)
-        return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
-    if (!introduce) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
-    if (!nickname) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
-
-    // 길이 체크
-    if (email.length > 30)
-        return res.send(response(baseResponse.SIGNUP_EMAIL_LENGTH));
-    if (introduce.length > 20)
+  // 길이 체크
+  if (email.length > 30)
+    return res.send(response(baseResponse.SIGNUP_EMAIL_LENGTH));
+  if (introduce.length > 20)
     return res.send(response(baseResponse.SIGNUP_INTRODUCE_LENGTH));
 
-    if (nickname.length > 7)
+  if (nickname.length > 7)
     return res.send(response(baseResponse.SIGNUP_NICKNAME_LENGTH));
-    if(password.length <6 ) {
+  if (password.length < 6) {
     return res.send(response(baseResponse.SIGNUP_PASSWORD_LENGTH));
-    }    
-    // 형식 체크 (by 정규표현식)
-    if (!regexEmail.test(email))
-        return res.send(response(baseResponse.SIGNUP_EMAIL_ERROR_TYPE));
+  }
+  // 형식 체크 (by 정규표현식)
+  if (!regexEmail.test(email))
+    return res.send(response(baseResponse.SIGNUP_EMAIL_ERROR_TYPE));
 
-    // 기타 등등 - 추가하기
+  // 기타 등등 - 추가하기
 
-
-    const signUpResponse = await userService.createUser(
-        nickname,address, age, gender, breed, dogAge, introduce, profileImage,status, email,password
-      );
-    return res.send(signUpResponse);
+  const signUpResponse = await userService.createUser(
+    nickname,
+    address,
+    age,
+    gender,
+    breed,
+    dogAge,
+    introduce,
+    profileImage,
+    status,
+    email,
+    password
+  );
+  return res.send(signUpResponse);
 };
 
 /**
@@ -64,21 +83,20 @@ exports.postUsers = async function (req, res) {
  * [GET] /app/users
  */
 exports.getUsers = async function (req, res) {
+  /**
+   * Query String: email
+   */
+  const email = req.query.email;
 
-    /**
-     * Query String: email
-     */
-    const email = req.query.email;
-
-    if (!email) {
-        // 유저 전체 조회
-        const userListResult = await userProvider.retrieveUserList();
-        return res.send(response(baseResponse.SUCCESS, userListResult));
-    } else {
-        // 유저 검색 조회
-        const userListByEmail = await userProvider.retrieveUserList(email);
-        return res.send(response(baseResponse.SUCCESS, userListByEmail));
-    }
+  if (!email) {
+    // 유저 전체 조회
+    const userListResult = await userProvider.retrieveUserList();
+    return res.send(response(baseResponse.SUCCESS, userListResult));
+  } else {
+    // 유저 검색 조회
+    const userListByEmail = await userProvider.retrieveUserList(email);
+    return res.send(response(baseResponse.SUCCESS, userListByEmail));
+  }
 };
 
 /**
@@ -87,18 +105,16 @@ exports.getUsers = async function (req, res) {
  * [GET] /app/users/{userId}
  */
 exports.getUserById = async function (req, res) {
+  /**
+   * Path Variable: userId
+   */
+  const userId = req.params.userId;
 
-    /**
-     * Path Variable: userId
-     */
-    const userId = req.params.userId;
+  if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
 
-    if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
-
-    const userByUserId = await userProvider.retrieveUser(userId);
-    return res.send(response(baseResponse.SUCCESS, userByUserId));
+  const userByUserId = await userProvider.retrieveUser(userId);
+  return res.send(response(baseResponse.SUCCESS, userByUserId));
 };
-
 
 // TODO: After 로그인 인증 방법 (JWT)
 /**
@@ -108,16 +124,14 @@ exports.getUserById = async function (req, res) {
  * body : email, passsword
  */
 exports.login = async function (req, res) {
+  const { email, password } = req.body;
 
-    const {email, password} = req.body;
+  // TODO: email, password 형식적 Validation
 
-    // TODO: email, password 형식적 Validation
+  const signInResponse = await userService.postSignIn(email, password);
 
-    const signInResponse = await userService.postSignIn(email, password);
-
-    return res.send(signInResponse);
+  return res.send(signInResponse);
 };
-
 
 /**
  * API No. 5
@@ -127,41 +141,31 @@ exports.login = async function (req, res) {
  * body : nickname
  */
 exports.patchUsers = async function (req, res) {
+  // jwt - userId 가 path variable :userId와 일치하는지
 
-    // jwt - userId 가 path variable :userId와 일치하는지
+  const userIdFromJWT = req.verifiedToken.userId;
+  console.log("userController:", req.verifiedToken);
+  const userId = req.params.userId;
+  const nickname = req.body.nickname;
 
-    const userIdFromJWT = req.verifiedToken.userId
+  if (userIdFromJWT != userId) {
+    res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+  } else {
+    if (!nickname)
+      return res.send(errResponse(baseResponse.USER_NICKNAME_EMPTY));
 
-    const userId = req.params.userId;
-    const nickname = req.body.nickname;
-
-    if (userIdFromJWT != userId) {
-        res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    } else {
-        if (!nickname) return res.send(errResponse(baseResponse.USER_NICKNAME_EMPTY));
-
-        const editUserInfo = await userService.editUser(userId, nickname)
-        return res.send(editUserInfo);
-    }
+    const editUserInfo = await userService.editUser(userId, nickname);
+    return res.send(editUserInfo);
+  }
 };
-
-
-
-
-
-
-
-
-
-
 
 /** JWT 토큰 검증 API
  * [GET] /app/auto-login
  */
 exports.check = async function (req, res) {
-    const userIdResult = req.verifiedToken.userId;
-    console.log(userIdResult);
-    return res.send(response(baseResponse.TOKEN_VERIFICATION_SUCCESS));
+  const userIdResult = req.verifiedToken.userId;
+  console.log(userIdResult);
+  return res.send(response(baseResponse.TOKEN_VERIFICATION_SUCCESS));
 };
 
 ///////////
@@ -179,23 +183,23 @@ exports.patchUser = async function (req, res) {
   const breed = req.body.breed;
   const dogAge = req.body.dogAge;
 
-  if (address){
+  if (address) {
     const editUserAdd = await userService.editUserAddress(userId, address);
     return res.send(editUserAdd);
   }
-  if (age){
+  if (age) {
     const editUserAge = await userService.editUserAge(userId, age);
     return res.send(editUserAge);
   }
-  if (gender){
+  if (gender) {
     const editUserGend = await userService.editUserGender(userId, gender);
     return res.send(editUserGend);
   }
-  if (breed){
+  if (breed) {
     const editUserBree = await userService.editUserBreed(userId, breed);
     return res.send(editUserBree);
   }
-  if (dogAge){
+  if (dogAge) {
     const editUserDog = await userService.editUserDogAge(userId, dogAge);
     return res.send(editUserDog);
   }
@@ -217,16 +221,16 @@ exports.getUserById = async function (req, res) {
 };
 
 /**
-* API Name : 5. 내가 쓴 글 조회 (GET)
-* [GET] /user/board/{userId}
-* * path variable : userId
-*/
+ * API Name : 5. 내가 쓴 글 조회 (GET)
+ * [GET] /user/board/{userId}
+ * * path variable : userId
+ */
 exports.getMyPost = async function (req, res) {
-   const userId = req.params.userId;
-   if (!userId) res.send(errResponse(baseResponse.USER_POSTING_NOT_EXIST));
+  const userId = req.params.userId;
+  if (!userId) res.send(errResponse(baseResponse.USER_POSTING_NOT_EXIST));
 
-   const MyPostList = await userProvider.retrieveUserPostList(userId);
-   return res.send(response(baseResponse.SUCCESS, MyPostList));
+  const MyPostList = await userProvider.retrieveUserPostList(userId);
+  return res.send(response(baseResponse.SUCCESS, MyPostList));
 };
 
 /**
@@ -238,15 +242,15 @@ exports.deleteUser = async function (req, res) {
   const userId = req.params.userId;
   if (!userId) res.send(errResponse(baseResponse.USER_USERID_NOT_EXIST));
 
-  const deleteUserInfo = await userService.deleteUser(userId)
+  const deleteUserInfo = await userService.deleteUser(userId);
   return res.send(response(baseResponse.SUCCESS, deleteUserInfo));
 };
 
 /**
-* API Name : 7. 내가 스크랩한 글 조회 (GET)
-* [GET] /user/board/scrap/:userId
-* * path variable : userId
-*/
+ * API Name : 7. 내가 스크랩한 글 조회 (GET)
+ * [GET] /user/board/scrap/:userId
+ * * path variable : userId
+ */
 exports.getMyScrap = async function (req, res) {
   const userId = req.params.userId;
   if (!userId) res.send(errResponse(baseResponse.USER_USERID_NOT_EXIST));
@@ -256,10 +260,10 @@ exports.getMyScrap = async function (req, res) {
 };
 
 /**
-* API Name : 8. 댓글 전체 조회 (GET)
-* [GET] /comments/:postId
-* * path variable : postId
-*/
+ * API Name : 8. 댓글 전체 조회 (GET)
+ * [GET] /comments/:postId
+ * * path variable : postId
+ */
 exports.getComment = async function (req, res) {
   const postId = req.params.postId;
   if (!postId) res.send(errResponse(baseResponse.USER_POSTING_NOT_EXIST));
@@ -273,7 +277,6 @@ exports.getComment = async function (req, res) {
  * [POST] /comments
  */
 exports.postComment = async function (req, res) {
-
   /**
    * Body: content
    */
@@ -287,12 +290,12 @@ exports.postComment = async function (req, res) {
 
   // 길이 체크
   if (content.length > 100)
-      return res.send(response(baseResponse.SIGNUP_CONTENT_LENGTH));
+    return res.send(response(baseResponse.SIGNUP_CONTENT_LENGTH));
 
   const signUpResponse = await userService.createComment(
-      postId,
-      userId,
-      content
+    postId,
+    userId,
+    content
   );
 
   return res.send(signUpResponse);
@@ -303,7 +306,6 @@ exports.postComment = async function (req, res) {
  * [POST] /comments/like/:postId/:commentId
  */
 exports.postLikeComment = async function (req, res) {
-
   /**
    * Body: userId
    */
@@ -316,9 +318,9 @@ exports.postLikeComment = async function (req, res) {
   if (!userId) res.send(response(baseResponse.USER_USERID_NOT_EXIST));
 
   const signUpResponse = await userService.createCommentLike(
-      postId,
-      commentId,
-      userId
+    postId,
+    commentId,
+    userId
   );
 
   return res.send(signUpResponse);
@@ -340,17 +342,20 @@ exports.deleteComment = async function (req, res) {
 };
 
 /**
-* API Name : 12. 대댓글 전체보기 (GET)
-* [GET] /comments/re/:postId/:commentId
-* * path variable : postId, commentId
-*/
+ * API Name : 12. 대댓글 전체보기 (GET)
+ * [GET] /comments/re/:postId/:commentId
+ * * path variable : postId, commentId
+ */
 exports.getRecomment = async function (req, res) {
   const postId = req.params.postId;
   const commentId = req.params.commentId;
   if (!postId) res.send(errResponse(baseResponse.USER_POSTING_NOT_EXIST));
   if (!commentId) res.send(errResponse(baseResponse.USER_COMMENT_NOT_EXIST));
 
-  const PostReommentList = await userProvider.retrievePostRecommList(postId, commentId);
+  const PostReommentList = await userProvider.retrievePostRecommList(
+    postId,
+    commentId
+  );
   return res.send(response(baseResponse.SUCCESS, PostReommentList));
 };
 
@@ -359,7 +364,6 @@ exports.getRecomment = async function (req, res) {
  * [POST] /comments/re/:postId/:commentId
  */
 exports.postRecomment = async function (req, res) {
-
   /**
    * Body: content
    */
@@ -375,13 +379,13 @@ exports.postRecomment = async function (req, res) {
 
   // 길이 체크
   if (content.length > 100)
-      return res.send(response(baseResponse.SIGNUP_CONTENT_LENGTH));
+    return res.send(response(baseResponse.SIGNUP_CONTENT_LENGTH));
 
   const signUpResponse = await userService.createRecomment(
-      postId,
-      commentId,
-      userId,
-      content
+    postId,
+    commentId,
+    userId,
+    content
   );
 
   return res.send(signUpResponse);
@@ -400,6 +404,10 @@ exports.deleteRecomment = async function (req, res) {
   if (!commentId) res.send(errResponse(baseResponse.USER_COMMENT_NOT_EXIST));
   if (!replyId) res.send(errResponse(baseResponse.USER_RECOMMENT_NOT_EXIST));
 
-  const deleteRecommentInfo = await userService.deleteRecomment(postId, commentId, replyId);
+  const deleteRecommentInfo = await userService.deleteRecomment(
+    postId,
+    commentId,
+    replyId
+  );
   return res.send(response(baseResponse.SUCCESS, deleteRecommentInfo));
 };
