5f3eac416dfc690217475e7712a3ffc93c05e374
youngduck <dudejr5839@naver.com>
2023-02-10 15:28:25 +0900

    safsfasf

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const { logger } = require("./winston");
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
+  host: "42.201.81.193",
+  user: "dogo",
   port: "3306",
-  password: "leejiwon0176!",
-  database: "dogo",
+  password: "umc1234",
+  database: "mydb",
 });
 
 module.exports = {
